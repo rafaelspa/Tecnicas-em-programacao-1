ebb533f9d58ce59605b2ef976e03428cf9c34db6
rafaelspa <rsdepaula67@gmail.com>
2026-08-28 19:27:58 -0300

    Remove coisas

--- main.cpp
+++ main.cpp
@@ -2,71 +2,8 @@
 
 using namespace std;
 
-// Declaração e implementação da classe Poligono.
-
-class Poligono {
-
-protected:
-
-     int numero_lados;
-
-public:
-
-     int getNumeroLados() const;
-
-};
-
-// Declaração e implementação da classe Retangulo.
-class Retangulo: protected Poligono {
-
-protected:
-    int base, altura;
-    int const N_LADOS = 4;
-
-public:
-    int getNumeroLados() const;
-    Retangulo(float, float);
-    float calcularArea();
-    float calcularPerimetro();
-
-};
-
-Retangulo::Retangulo(float base, float altura){
-    this->base = base;
-    this->altura = altura;
-    this->numero_lados = N_LADOS;
-}
-
-int Retangulo::getNumeroLados() const {
-    return this->numero_lados;
-}
-
-float Retangulo::calcularArea() {
-    return this->base * this->altura;
-}
-
-float Retangulo::calcularPerimetro() {
-    return 2 * base + 2 * altura;
-}
-
-// Implementação de método main.
-
 int main() {
-
-     float base, altura;
-
-     cin >> base;
-
-     cin >> altura;
-
-     Retangulo retangulo(base, altura);
-
-     cout << retangulo.getNumeroLados();
-
-     cout << retangulo.calcularArea();
-
-     cout << retangulo.calcularPerimetro();
-
-     return 0;
+    cout << "Hello" << endl;
+    return 0;
 
 }
